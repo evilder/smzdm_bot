73470fe53f07afd8d38dfd1553462fa0865d54c7
evilder <37235837+evilder@users.noreply.github.com>
2022-11-10 13:37:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=l4L6fYvH2FkncGxc6t5OI44; device_id=213070643316578097310720167da99292d99ac2461a96c2ea866bb0ff; r_sort_type=score; __jsluid_s=02088a10cdc58c9735f8c2578ef362a0; homepage_sug=d; shequ_pc_sug=b; _zdmA.vid=*; footer_floating_layer=0; ad_date=10; ad_json_feed={}; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1666012809,1666097239,1667362504,1668057849; sess=BA-05kIXn+ekNtPWucesodKRICGTVp+eRrrlun5cmWq8O0fbQP7ycFWVxweTvDXLbMHGIsLWI89eaKk7oXxoJil3U6skC+X5qqTR1v1lz5D/CaxQ9scjSH3kbeH; user=mafia310|1967374126; _zdmA.time=1668057868887.18483.https://www.smzdm.com/; _zdmA.vid=*; smzdm_id=1967374126; smzdm_user_view=96EF2FD681747B8724CCC9104A5741EF; smzdm_user_source=F88496657FFB58130ABB30D909BCF51C; sensorsdata2015jssdkcross={"distinct_id":"1967374126","first_id":"181fd290007407-0f0d02479dede8-74492d21-1327104-181fd290008359","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"$device_id":"181fd290007407-0f0d02479dede8-74492d21-1327104-181fd290008359"}; _zdmA.uid=ZDMA.CxPv5nSBO.1668058045.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1668058045; bannerCounter=[{"number":3,"surplus":1},{"number":1,"surplus":1},{"number":1,"surplus":1},{"number":1,"surplus":1},{"number":3,"surplus":1},{"number":1,"surplus":1}]; amvid=a501e432a0e45fff253b36c453f4e200; _zdmA.time=1668058464764.0.https://www.smzdm.com/'
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = ''
+SERVERCHAN_SECRETKEY = 'SCT10258TUM2uUSHfwVC3h3KNkq0c3pvV'
